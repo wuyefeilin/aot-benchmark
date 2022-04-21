f1d2b7af4a79fbb25a18ba6cd51ede914e86bd9f
wuyefeilin <chenguowei01@baidu.com>
2022-04-21 15:08:51 +0800

    update rs1010_aotl.py

--- configs/models/rs101_aotl.py
+++ configs/models/rs101_aotl.py
@@ -4,7 +4,7 @@ from .default import DefaultModelConfig
 class ModelConfig(DefaultModelConfig):
     def __init__(self):
         super().__init__()
-        self.MODEL_NAME = 'R101_AOTL'
+        self.MODEL_NAME = 'RS101_AOTL'
 
         self.MODEL_ENCODER = 'resnest101'
         self.MODEL_ENCODER_PRETRAIN = './pretrain_models/resnest101-22405ba7.pth'  # https://github.com/zhanghang1989/ResNeSt/releases/download/weights_step1/resnest101-22405ba7.pth
