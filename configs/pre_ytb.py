bb1cdbc3e647289c42b9bc64b68dbfe939d77dbc
wuyefeilin <chenguowei01@baidu.com>
2022-05-11 19:40:46 +0800

    update some

--- configs/pre_ytb.py
+++ configs/pre_ytb.py
@@ -5,6 +5,8 @@ from .default import DefaultEngineConfig
 class EngineConfig(DefaultEngineConfig):
     def __init__(self, exp_name='default', model='AOTT'):
         super().__init__(exp_name, model)
+        # self.STAGE_NAME = 'PRE_YTB_ft_100000'
+        # self.STAGE_NAME = 'PRE_YTB_ft_95000'
         self.STAGE_NAME = 'PRE_YTB'
 
         self.init_dir()
@@ -12,7 +14,8 @@ class EngineConfig(DefaultEngineConfig):
         pretrain_stage = 'PRE'
         pretrain_ckpt = 'save_step_100000.pth'
         self.PRETRAIN_FULL = True  # if False, load encoder only
-        self.PRETRAIN_MODEL = os.path.join(self.DIR_ROOT, 'result',
-                                           self.EXP_NAME, pretrain_stage,
-                                           'ema_ckpt', pretrain_ckpt)
-        # self.PRETRAIN_MODEL = 'pretrain_models/SwinB_AOTL_PRE.pth'
+        # self.PRETRAIN_MODEL = os.path.join(self.DIR_ROOT, 'result',
+        #                                    self.EXP_NAME, pretrain_stage,
+        #                                    'ema_ckpt', pretrain_ckpt)
+        # self.PRETRAIN_MODEL = 'results/result/default_RS101_AOTL/PRE_YTB/ema_ckpt/save_step_100000.pth'
+        self.PRETRAIN_MODEL = 'results/result/default_SwinB_AOTL/PRE_YTB/ema_ckpt/save_step_95000.pth'
--- train_eval.sh
+++ train_eval.sh
@@ -7,8 +7,8 @@ gpu_num="4"
 # model="aots"
 # model="aotb"
 # model="aotl"
-# model="r50_aotl"
-model="swinb_aotl"
+model="r50_aotl"
+# model="swinb_aotl"
 # model="rs101_aotl"
 
 ## Training ##
@@ -24,7 +24,14 @@ model="swinb_aotl"
 # 	--exp_name ${exp} \
 # 	--stage ${stage} \
 # 	--model ${model} \
-# 	--gpu_num ${gpu_num}
+# 	--gpu_num ${gpu_num}  
+
+# stage="pre_blk"
+# python tools/train.py --amp \
+# 	--exp_name ${exp} \
+# 	--stage ${stage} \
+# 	--model ${model} \
+# 	--gpu_num ${gpu_num} 
 
 stage="pre_blk_ytb"
 python tools/train.py --amp \
@@ -57,13 +64,115 @@ python tools/train.py --amp \
 dataset="youtubevos2019"
 split="val"  # or "val_all_frames"
 python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num}
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} 
 
-stage="pre_blk_ytb"
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-gpu_num="4"
 dataset="youtubevos2019"
 split="val"  # or "val_all_frames"
 python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-	--flip --ms 0.5 0.75 1.0 1.25 1.5
+	--ckpt_step 99000 
+
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 98000 
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 97000 
+
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 96000 
+
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 95000 
+
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 94000 
+
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 93000 
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 92000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 91000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 90000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 89000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 88000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 87000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 86000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 85000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 84000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 83000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 82000 \
+
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 81000 \
+
+# dataset="youtubevos2019"
+# split="val"  # or "val_all_frames"
+# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+# 	--flip --ms  0.85 1.0 1.3 \
+
+
+
+# # model="r50_aotl"
+# model="swinb_aotl"
+# # model="rs101_aotl"
+# exp="default"
+# stage="pre_ytb"
+# export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+# gpu_num="8"
+# dataset="youtubevos2019"
+# split="val"  # or "val_all_frames"
+# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+# 	--flip --ms  0.85 1.0 1.3 \
+# 	--ckpt_step 95000
