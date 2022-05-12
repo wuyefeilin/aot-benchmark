b99293603e84601995002e6fe2840a09f282d453
wuyefeilin <chenguowei01@baidu.com>
2022-05-12 21:51:39 +0800

    update train_eval.sh

--- train_eval.sh
+++ train_eval.sh
@@ -26,134 +26,100 @@ python tools/train.py --amp \
 	--model ${model} \
 	--gpu_num ${gpu_num}  
 
-# stage="pre_blk"
-# python tools/train.py --amp \
-# 	--exp_name ${exp} \
-# 	--stage ${stage} \
-# 	--model ${model} \
-# 	--gpu_num ${gpu_num} 
 
-# stage="pre_blk_ytb"
-# python tools/train.py --amp \
-# 	--exp_name ${exp} \
-# 	--stage ${stage} \
-# 	--model ${model} \
-# 	--gpu_num ${gpu_num}
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} 
 
-## Evaluation ##
-# dataset="davis2017"
-# split="test"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num}
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 99000 
 
-# dataset="davis2017"
-# split="val"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num}
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 98000 
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 97000 
 
-# dataset="davis2016"
-# split="val"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num}
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 96000 
 
-# dataset="youtubevos2018"
-# split="val"  # or "val_all_frames"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num}
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 95000 
 
-# dataset="youtubevos2019"
-# split="val"  # or "val_all_frames"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} 
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 94000 
 
-# dataset="youtubevos2019"
-# split="val"  # or "val_all_frames"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 99000 
+dataset="youtubevos2019"
+split="val"  # or "val_all_frames"
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 93000 
 
-# dataset="youtubevos2019"
-# split="val"  # or "val_all_frames"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 98000 
-# dataset="youtubevos2019"
-# split="val"  # or "val_all_frames"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 97000 
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 92000 \
 
-# dataset="youtubevos2019"
-# split="val"  # or "val_all_frames"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 96000 
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 91000 \
 
-# dataset="youtubevos2019"
-# split="val"  # or "val_all_frames"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 95000 
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 90000 \
 
-# dataset="youtubevos2019"
-# split="val"  # or "val_all_frames"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 94000 
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 89000 \
 
-# dataset="youtubevos2019"
-# split="val"  # or "val_all_frames"
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 93000 
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 88000 \
 
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 92000 \
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 87000 \
 
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 91000 \
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 86000 \
 
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 90000 \
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 85000 \
 
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 89000 \
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 84000 \
 
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 88000 \
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 83000 \
 
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 87000 \
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 82000 \
 
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 86000 \
-
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 85000 \
-
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 84000 \
-
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 83000 \
-
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 82000 \
-
-# python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
-# 	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
-# 	--ckpt_step 81000 \
+python tools/eval.py --exp_name ${exp} --stage ${stage} --model ${model} \
+	--dataset ${dataset} --split ${split} --gpu_num ${gpu_num} \
+	--ckpt_step 81000 \
 
 # dataset="youtubevos2019"
 # split="val"  # or "val_all_frames"
